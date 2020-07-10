a89e156614b1f2b7ddc2773c95b0281fbba99b80
Benz <Benzeman097-nafazbenzema@gmail.com>
2020-07-10 14:04:41 +0530

    JWT  implemented

--- web-portal/src/main/java/com/benz/web/config/SecurityConfiguration.java
+++ web-portal/src/main/java/com/benz/web/config/SecurityConfiguration.java
@@ -1,25 +1,29 @@
 package com.benz.web.config;
 
+import com.benz.web.filter.JwtRequestFilter;
 import com.benz.web.services.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
-//@EnableOAuth2Sso
 public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -40,19 +44,29 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
 
    }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
 
-        http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("USER","ADMIN")
-                .antMatchers("/*").permitAll().and().formLogin();
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/admin").hasRole(Permission.ADMIN)
+                .antMatchers("/user").hasAnyRole(Permission.USER,Permission.ADMIN)
+                .antMatchers("/*").permitAll().anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+       http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
 //         http.csrf().disable()
 //                 .authorizeRequests().antMatchers("/login").permitAll()
 //                 .anyRequest().authenticated();
 
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
--- web-portal/src/main/java/com/benz/web/controller/HomeController.java
+++ web-portal/src/main/java/com/benz/web/controller/HomeController.java
@@ -1,14 +1,31 @@
 package com.benz.web.controller;
 
+import com.benz.web.model.AuthenticationRequest;
+import com.benz.web.model.AuthenticationResponse;
 import com.benz.web.services.UserDetailsService;
+import com.benz.web.services.impl.UserDetails;
+import com.benz.web.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController{
 
+     @Autowired
+     private AuthenticationManager authManager;
 
+     @Autowired
+     private UserDetailsService userDetailsService;
+
+     @Autowired
+     private JwtUtil jwtUtil;
 
     @RequestMapping("/admin")
     public String getAdmin()
@@ -29,5 +46,25 @@ public class HomeController{
     }
 
 
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> createAuthentication(@RequestBody AuthenticationRequest authRequest) throws Exception
+    {
+        try {
+            authManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
+
+
+            final UserDetails userDetails = (UserDetails)userDetailsService.loadUserByUsername(authRequest.getUserName());
+
+            final String jwt=jwtUtil.generateToken(userDetails);
+
+            return ResponseEntity.ok(new AuthenticationResponse(jwt));
+
+
+
+        }catch (BadCredentialsException bx)
+        {
+            throw new Exception("Username or Password is invalid",bx);
+        }
+    }
 
 }
